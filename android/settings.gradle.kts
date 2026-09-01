9f93d16e822c173f717ec9c9ffa409029af072ed
smajicnihad-pixel <smajic.nihad@gmail.com>
2026-09-01 12:57:07 +0000

    Reset settings.gradle.kts to standard Flutter template configuration

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -2,28 +2,15 @@ pluginManagement {
     val flutterSdkPath = run {
         val properties = java.util.Properties()
         file("local.properties").inputStream().use { properties.load(it) }
-        val flutterSdkPath = properties.getProperty("flutter.sdk")
-        assert(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-        flutterSdkPath
+        properties.getProperty("flutter.sdk") ?: throw GradleException("Flutter SDK not found in local.properties")
     }
-
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
-
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
 }
 
 plugins {
-    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
-        id("com.android.application") version "8.11.1" apply false
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.11.1" apply false
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 include(":app")
-
-
-project(":flutter").projectDir = file("/packages/flutter_tools/gradle")
