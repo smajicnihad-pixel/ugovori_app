c9782b4c4e9743e65f8da7e358cab3577083720d
smajicnihad-pixel <smajic.nihad@gmail.com>
2026-09-04 10:52:12 +0000

    Povecana verzija desugar_jdk_libs na 2.1.4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -70,5 +70,5 @@ flutter {
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
     implementation("com.google.firebase:firebase-analytics")
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
